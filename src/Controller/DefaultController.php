a06f25d2e4ea300afe2aca858c8bc8fee8f716a0
realvaz <test@example.com>
2021-06-11 11:51:15 +0200

    Añadir el primer controlador

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController 
+{
+    /**
+     * @Route("/default", name="default_index")
+     * 
+     * La clase ruta debe estar precedida en los comentario por una arroba.
+     * El primer parámetro de Route es la URL a la que queremos asociar la acción.
+     * El segundo parámetro de Route es el nombre que queremos dar a la ruta.
+     */
+    public function index(): Response
+    {
+        // Una acción siempre debe devolver una respesta.
+        // Por defecto deberá ser un objeto de la clase,
+        // Symfony\Component\HttpFoundation\Response
+        return new Response('hola');
+    }
+}
