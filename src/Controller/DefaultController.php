20c6296c87c9e803db096b8af6373462c837c433
realvaz <test@example.com>
2021-06-11 13:06:07 +0200

    Añadir plantilla para la accion index del controlador Default

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController 
+// AbstractController es un controlador de Symfony
+// que pone a disposición nuestra multitud de características.
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/default", name="default_index")
@@ -21,6 +24,15 @@ class DefaultController
         // Una acción siempre debe devolver una respesta.
         // Por defecto deberá ser un objeto de la clase,
         // Symfony\Component\HttpFoundation\Response
-        return new Response('hola');
+
+        // render() es un método hereado de AbstractController
+        // que devuelve el contenido declarado en una plantillas de Twig.
+        // https://twig.symfony.com/doc/3.x/templates.html
+        
+        $name = 'Zacarías';
+        
+        return $this->render('default/index.html.twig', [
+           'nombre' => $name
+        ]);
     }
 }
